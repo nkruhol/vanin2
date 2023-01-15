256a7a48114ebf1876a67093be7e6fb8ee74c024
kruhol <n.kruhol@gmail.com>
2023-01-15 14:58:56 +0100

    added editors for article rules und letters

--- client/src/app/pages/article-rules/article-rules.component.ts
+++ client/src/app/pages/article-rules/article-rules.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { filter, map } from 'rxjs/operators';
+import { selectLanguage, selectSiteOptions } from 'src/app/storage/app/app.selectors';
+import { LanguageEnum } from 'src/app/storage/app/app.state';
+import { IStore } from 'src/app/storage/store';
 
 @Component({
   selector: 'app-article-rules',
   templateUrl: './article-rules.component.html',
   styleUrls: ['./article-rules.component.scss']
 })
-export class ArticleRulesComponent { }
+export class ArticleRulesComponent {
+
+  language$ = this.store.select(selectLanguage);
+  page$ = this.store.select(selectSiteOptions)
+    .pipe(
+      filter(i => !!i),
+      filter(i => !!i.pages),
+      map(i => i.pages.rules),
+    );
+
+  constructor(
+    private store: Store<IStore>,
+  ) { }
+
+  LanguageEnum = LanguageEnum;
+}
